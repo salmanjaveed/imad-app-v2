eb543164999b47c924d3407cb3931adc86ab0cec
salmanjaveed <salmanjaveed@gmail.com>
2017-02-02 13:37:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,7 @@
-console.log('Loaded!');
+var madimover = get.ElementbyId("madi");
+madimover = Onclick( moveRight() );
+function moveRight() {
+    
+    madimover.style.margin-right = 100px;
+    return true;
+}
