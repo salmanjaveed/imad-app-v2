0d02f6897a1573623943ab6e82763b060ba3cafd
salmanjaveed <salmanjaveed@gmail.com>
2017-02-05 15:11:12 +0530

    [imad-console] Updates s1.js

--- s1.js
+++ s1.js
@@ -10,6 +10,10 @@
 			  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 			});
 
+            app.get('/index2.html', function (req, res) {
+			  res.sendFile(path.join(__dirname, 'ui', 'index2.html'));
+			});
+
 			app.get('/ui/style.css', function (req, res) {
 			  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 			});
