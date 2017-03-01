1d627989a74a97d57e7d51f4a617eb88f7777703
salmanjaveed <salmanjaveed@gmail.com>
2017-03-01 16:42:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -141,12 +141,12 @@ app.get('/articles/:articlename', function (req, res) {
  });
 });
 
-
+/*
 app.get('/:articleName', function (req, res) {
  var articleName = req.params.articleName;
  res.send(createTemplate(articles[articleName]));
 });
-
+*/
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
