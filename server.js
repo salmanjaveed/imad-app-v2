ad4827c0f29a49096f2d856fec2fd868a4ff3607
salmanjaveed <salmanjaveed@gmail.com>
2017-03-01 06:43:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -135,5 +135,5 @@ app.get('/favicon.ico', function (req, res) {
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(process.env.PORT || 8080, function () {
-  console.log(`IMAD course app listening on port ${process.env.PORT || 5000}!`);
+  console.log(`IMAD course app listening on port ${process.env.PORT || 8080}!`);
 });
