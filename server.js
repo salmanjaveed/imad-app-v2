879d80e5beb4c20aeae4d93bf3f926420e855197
salmanjaveed <salmanjaveed@gmail.com>
2017-03-01 19:54:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var Pool = require('pg').Pool;
 
 var config = {
     user: "salmanjaveed",
-    host: "localhost",
+    host: "db.imad.hasura-app.io",
     database: "salmanjaveed",
     port: "5432",
     password: process.env.DB_PASSWORD
