c2e9154886ab3701672304376c3801f3a46e45b0
Salman Javeed <salmanjaveed@gmail.com>
2017-03-09 21:33:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -204,6 +204,23 @@ app.post('/create-user', function (req, res) {
    });
 });
 
+
+app.get('/check-login', function (req, res) {
+   if (req.session && req.session.auth && req.session.auth.userId) {
+       // Load the user object
+       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
+           if (err) {
+              res.status(500).send(err.toString());
+           } else {
+              res.send(result.rows[0].username);    
+           }
+       });
+   } else {
+       res.status(400).send('You are not logged in');
+   }
+});
+
+
 app.post('/login', function (req, res) {
    var username = req.body.username;
    var password = req.body.password;
@@ -237,25 +254,12 @@ app.post('/login', function (req, res) {
    });
 });
 
-app.get('/check-login', function (req, res) {
-   if (req.session && req.session.auth && req.session.auth.userId) {
-       // Load the user object
-       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
-           if (err) {
-              res.status(500).send(err.toString());
-           } else {
-              res.send(result.rows[0].username);    
-           }
-       });
-   } else {
-       res.status(400).send('You are not logged in');
-   }
-});
 
 app.get('/logout', function (req, res) {
    delete req.session.auth;
    res.send(`<html><body>Logged out!<br/><br/><a href="/">Back to home</a></body></html>`);
 });
+
 
 var pool = new Pool(config);
 
