713a11c57095e8b0d3e33383b7dc24d91e571836
Salman Javeed <salmanjaveed@gmail.com>
2017-03-10 10:12:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,23 +64,6 @@ function createTemplate(data) {
 
 
 
-app.get('/checklogin', function (req, res) {
-   if (req.session && req.session.auth && req.session.auth.userId) {
-       // Load the user object
-       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
-           if (err) {
-              res.status(500).send(err.toString());
-           } else {
-              res.send(result.rows[0].username);    
-           }
-       });
-   } else {
-       res.status(400).send('You are not logged in');
-   }
-});
-
-
-
 var counter = 0;
 app.get('/counter', function (req, res) {
     counter = counter +  1;
