c96b1d7aca160d5aeaab2a8bda51a3321a4b39cc
Salman Javeed <salmanjaveed@gmail.com>
2017-03-10 23:23:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -263,9 +263,9 @@ app.post('/login', function (req, res) {
 
 
 app.get('/check-login', function (req, res) {
-    console.log(req.session, req.session.auth, req.session.auth.userId);
+    
    if (req.session && req.session.auth && req.session.auth.userId) {
- 
+    console.log(req.session, req.session.auth, req.session.auth.userId);
        // Load the user object
        pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
            if (err) {
