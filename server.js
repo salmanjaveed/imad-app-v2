8d86519f3bcd701ecda22952d29a2856c52cf558
Salman Javeed <salmanjaveed@gmail.com>
2017-03-13 23:39:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,14 +26,6 @@ app.use(bodyParser.json());
 
 var pool = new Pool(config);
 
-
-/*
-app.use(session({
-    'secret': 'someRandomSecretValue',
-    'cookie': { maxAge: 1000 * 60 * 60 * 24 * 30}
-}));
-*/
-
 app.use(session({ secret: 'someRandomSecretValuet', cookie: { maxAge: 1000 * 60 * 60 * 24 * 30 }, resave: true, saveUninitialized: true }));
 
 app.get('/', function (req, res) {
@@ -81,33 +73,7 @@ function createTemplate(data) {
     var title = data.title;
     var heading = data.heading;
     var content = data.content;
-  /*      
-    var htmlTemplate = `
-    <html>
-    <head>
-            <meta name=viewport content="width=device-width, initial-scale=1.0">
-            <link href="/ui/style.css" rel="stylesheet" />
-            <title>
-                ${title}
-            </title>
-    </head>    
-    <body>
-        <div class="container">
-            <div class="linker-div">
-                <a href="/" class="linker">Home</a>
-               
-            </div>
-        
-            <div class="container2">
-                    <h1>${heading}</h1>
-                    ${content}
-            </div>
-        </div>      
-       
-    </body>    
-    </html>`;
- 
- 
+/*
  var htmlTemplate = ` ${pagefile} `;
  console.log(htmlTemplate);
  */
@@ -348,109 +314,10 @@ function createTemplate(data) {
     return htmlTemplate;
 }
 
-/*
-
-var counter = 0;
-app.get('/counter', function (req, res) {
-    counter = counter +  1;
-    res.send(counter.toString()); 
-});
-
-var names = [];
-var comments = [];
-app.get('/comment', function (req, res) {
-    
-    var name = req.query.name;
-    var comment = req.query.comment;
-    comment = '<strong>' +  name + '</strong>' + ' says :' + comment;
-    names.push(name);
-    comments.push(comment);
-    res.send(JSON.stringify(comments)); 
- //   res.send({'comment': JSON.stringify(comments), 'name': JSON.stringify(names)});
-});
-
-var pool = new Pool(config);
-
-app.get('/articles/:articlename', function (req, res) {
- pool.query( "SELECT * FROM article WHERE title = '" + req.params.articlename +"'", function( err, result) {
-     if (err) {
-         res.status(500).send(err.toString());
-     }
-     else {
-         if (result.rows.length === 0)
-         {
-             res.status(404).send('Article Not Found!!!');
-         }
-         else {
-             
-             res.send(createTemplate(result.rows[0]));
-         }
-     }
- });
-});
-*/
-
-/*
-app.get('/:articleName', function (req, res) {
- var articleName = req.params.articleName;
- res.send(createTemplate(articles[articleName]));
-});
-*/
 
 function hash(input, salt) {
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
- /*   var returnString = `
-    <table width="150" border="0" align="center" cellpadding="5" cellspacing="0" style="border-collapse:collapse;border:1px dotted #8c0400;font-family:arial,helvetica,sans-serif;">
-        <tbody>
-            <tr>
-                <td style="background-color:#ffe8ee;">
-            
-                    Function
-                            
-                </td>
-                <td>
-                        
-                     pbdkf2Sync
-                            
-                </td>
-            </tr>
-            <tr>
-                <td style="background-color:#ffe8ee;">
-                            
-                     Salt Used
-                            
-                </td><td>
-                            
-                      ${salt}
-                            
-                </td></tr><tr><td style="background-color:#ffe8ee;">
-                    
-                      Iterations
-                            
-                </td><td>
-                            
-                     10000
-                            
-                </td>
-            </tr>
-            <tr>
-                <td style="background-color:#ffe8ee;">
-                            
-                     Hash String
-                            
-                </td>
-                <td> 
-                    <div style="word-wrap: break-word;">` + 
-                            
-                                hashed.toString("hex") + `</div>
-                            
-                </td>
-            </tr>
-        </tbody>
-    </table> `;
-    */
-    
- //   return (returnString);
+
   return ['pbkdf2Sync', "10000", salt,  (hashed.toString('hex'))].join('$');
 }
 
@@ -588,57 +455,6 @@ app.post(`/submit-comment/:articleName`, function (req, res) {
     }
 });
  
- /*
-var articles = {
-    'article-one': { title: 'This is the first title 1/3 in a series',
-                heading: 'This is the first heading 1/3 in a series',
-                content: `
-            <p>First paragraph</p>
-            <p>
-                This is the content of the paragraph which will be displayed on the page of the website that we are creating. Although, this seems to be the first of many files, that we are going use, we have to be very careful in doing so.
-            </p>
-            <p>
-                While still at it, why not have another paragraph to go with the first one. We Just want to see if the files that we are uploading on github works well with the git and the imad host.
-                
-            </p>
-`
-    },
-    'article2': { title: 'This is the second title 2/3 in a series',
-                heading: 'This is the first heading 2/3 in a series',
-                content: `
-            <p>Second paragraph</p>
-            <p>
-                This is the content of the paragraph which will be displayed on the page of the website that we are creating. Although, this seems to be the first of many files, that we are going use, we have to be very careful in doing so.
-            </p>
-            <p>
-                While still at it, why not have another paragraph to go with the first one. We Just want to see if the files that we are uploading on github works well with the git and the imad host.
-                
-            </p>
-`
-    },
-    'article-3': { title: 'This is the third title 3/3 in a series',
-                heading: 'This is the first heading 3/3 in a series',
-                content: `
-            <p>Third paragraph</p>
-            <p>
-                This is the content of the paragraph which will be displayed on the page of the website that we are creating. Although, this seems to be the first of many files, that we are going use, we have to be very careful in doing so.
-            </p>
-            <p>
-                While still at it, why not have another paragraph to go with the first one. We Just want to see if the files that we are uploading on github works well with the git and the imad host.
-                
-            </p>
-`
-    }
-};
-*/
-
-/*
-app.get('/aa', function (req, res) {
-  res.send(req.baseUrl+" <br> "+req.body+" <br> "+req.cookies+" <br> "+req.fresh+" <br> "+req.hostname+" <br> "+req.ip+" <br> "+req.ips+" <br> "+req.originalUrl+" <br> "+req.params+" <br> "+req.path+" <br> "+req.protocol+" <br> "+req.query+" <br> "+req.route+" <br> "+req.secure+" <br> "+req.signedCookies+" <br> "+req.stale+" <br> "+req.subdomains+" <br> "+req.xhr+" <br> ");
-});
-*/
-
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
