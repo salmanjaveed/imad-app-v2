e4401ed87122bddc10bfb32763f47c482d0dfee2
Salman Javeed <salmanjaveed@gmail.com>
2017-03-15 22:52:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -144,7 +144,7 @@ app.get('/articles/:articleName', function (req, res) {
             res.status(404).send('Article not found');
         } else {
             var articleData = result.rows[0];
-            req.setRequestHeader("Content-type", "text/html");
+            //req.setRequestHeader("Content-type", "text/html");
             res.send(createTemplate(articleData));
            
         }
