7adca16767738636ad71b9957e1421c79723f9e8
Salman Javeed <salmanjaveed@gmail.com>
2017-03-15 08:12:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -142,7 +142,7 @@ app.get('/articles/:articleName', function (req, res) {
         } else {
             var articleData = result.rows[0];
             res.send(createTemplate(articleData));
-            loadLogin();
+           
         }
     }
   });
