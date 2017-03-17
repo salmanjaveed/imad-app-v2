bce79510b63f923520b45565d2031bc0b4b8c9ae
Salman Javeed <salmanjaveed@gmail.com>
2017-03-17 23:31:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -168,7 +168,7 @@ app.get('/get-num-comments/:articleName', function (req, res) {
         if (result.rows.length === 0) {
             res.status(404).send('Article not found');
         } else {
-            console.log("all: " + result.rows + " 0: " + result.rows[0]);
+            
             res.send(JSON.stringify(result.rows[0]));
            
         }
