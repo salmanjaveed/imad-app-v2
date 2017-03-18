403a3ee7b90605677903ab7ce97f72dfa9ba8c8e
Salman Javeed <salmanjaveed@gmail.com>
2017-03-18 19:46:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -276,6 +276,21 @@ app.post('/submit-comment/:articleName', function (req, res) {
    });
 });
 
+ app.get('/get-footercomments', function (req, res) {
+   // make a select request
+   // return a response with the results - select all counts of articles, comments and users
+   pool.query('SELECT comment.*, "user".username FROM comment, "user" WHERE comment.user_id = "user".id', function (err, result) {
+       
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          
+          res.send(JSON.stringify(result.rows));
+      }
+   });
+});
+
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
