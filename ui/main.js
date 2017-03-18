b2a4a320f6545df121a1d76da4833401850f95ba
Salman Javeed <salmanjaveed@gmail.com>
2017-03-19 01:18:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -162,7 +162,9 @@ function loadLoginForm () {
                   loadLoginforComment();
               } else if (request.status === 403) {
                   submit.value = 'Invalid credentials. Try again?';
-                  document.getElementById('login-area').innerHTML = '';
+                  if ( document.body.contains('login-area') ) {
+                    document.getElementById('login-area').innerHTML = '';
+                }
               } else if (request.status === 500) {
                   alert('Something went wrong on the server' + request.status.toString());
                   submit.value = 'Login';
@@ -294,7 +296,9 @@ function logout() {
     request.onreadystatechange = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
             if (request.status === 200) {
-               
+                if ( document.body.contains('login-area') ) {
+                    document.getElementById('login-area').innerHTML = '';
+                }
                 loadLoginForm();
             }
             
