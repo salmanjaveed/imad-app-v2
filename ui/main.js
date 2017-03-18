7ec923598dc6015d0ceccfe8ad115888d601e5b5
Salman Javeed <salmanjaveed@gmail.com>
2017-03-19 00:25:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -159,6 +159,7 @@ function loadLoginForm () {
               // Take some action
               if (request.status === 200) {
                   submit.value = 'Success!';
+                  loadLoginforComment();
               } else if (request.status === 403) {
                   submit.value = 'Invalid credentials. Try again?';
               } else if (request.status === 500) {
