08da2b5db633e7bd9c4d8aec8d42794ea3ee578c
Salman Javeed <salmanjaveed@gmail.com>
2017-03-18 14:01:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -198,7 +198,7 @@ function loadLoginForm () {
               if (request.status === 200) {
                  // alert('User created successfully');
                   register.value = 'Registered!';
-                 // loadLoggedInUser(username);
+                 loadRegisteredUser( username, password );
                  loadLogin();
                   
               } else {
@@ -220,6 +220,44 @@ function loadLoginForm () {
     
     };
 }
+
+function loginRegisteredUser ( psername, password ){
+    
+
+      var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  submit.value = 'Success!';
+              } else if (request.status === 403) {
+                  submit.value = 'Invalid credentials. Try again?';
+              } else if (request.status === 500) {
+                  alert('Something went wrong on the server' + request.status.toString());
+                  submit.value = 'Login';
+              } else {
+                  alert('Something went wrong on the server' + request.status.toString());
+                  submit.value = 'Login';
+              }
+              loadLogin();
+          }  
+          // Not done yet
+        };
+        
+        // Make the request
+      //  var username = document.getElementById('username').value;
+    //    var password = document.getElementById('password').value;
+        //console.log(username);
+        //console.log(password);
+        request.open('POST', '/login', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        submit.value = 'Logging in...';
+        
+    }
+    
 
 function loadLoggedInUser (username) {
     var loginArea = document.getElementById( 'login-area');
