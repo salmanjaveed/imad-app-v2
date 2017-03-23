9dffa4bc8c1bb06cdd0b6726c1ed59184a913844
Salman Javeed <salmanjaveed@gmail.com>
2017-03-23 21:21:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -481,7 +481,7 @@ function loadArticleComposeForm () {
         request.open('POST', '/submit-article', true);
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify({title: title, article: article}));  
-        register.value = 'Sending...';
+        submit.value = 'Sending...';
         /*
         request.open('POST', '/submit-article/' + currentArticleTitle , true);
         request.setRequestHeader('Content-Type', 'application/json');
