2a0f13ff53a62a61c3bdbf9937e5d8ed1ef3b522
Salman Javeed <salmanjaveed@gmail.com>
2017-04-05 18:02:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -169,48 +169,60 @@ app.post('/create-user', function (req, res) {
    // JSON
    var username = req.body.username;
    var password = req.body.password;
-   var salt = crypto.randomBytes(128).toString('hex');
-   var dbString = hash(password, salt);
-   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
-      if (err) {
-          res.status(500).send(err.toString());
-      } else {
-          res.send('User successfully created: ' + username);
+     if ( username && password) { // that is username and password are not empty
+   
+           var salt = crypto.randomBytes(128).toString('hex');
+           var dbString = hash(password, salt);
+           pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
+              if (err) {
+                  res.status(500).send(err.toString());
+              } else {
+                  res.send('User successfully created: ' + username);
+              }
+           });
+     }
+    else {
+           res.status(403).send('username/password is invalid');
       }
-   });
 });
 
 app.post('/login', function (req, res) {
    var username = req.body.username;
    var password = req.body.password;
    
-   pool.query('SELECT * FROM "user" WHERE username = $1', [username], function (err, result) {
-      if (err) {
-         // alert(res.send(err.toString()));
-          res.status(500).send(err.toString());
-      } else {
-          if (result.rows.length === 0) {
-              res.status(403).send('username/password is invalid');
+   if ( username && password) { // that is username and password are not empty
+      
+       pool.query('SELECT * FROM "user" WHERE username = $1', [username], function (err, result) {
+          if (err) {
+             // alert(res.send(err.toString()));
+              res.status(500).send(err.toString());
           } else {
-              // Match the password
-              var dbString = result.rows[0].password;
-              var salt = dbString.split('$')[2];
-              var hashedPassword = hash(password, salt); // Creating a hash based on the password submitted and the original salt
-              if (hashedPassword === dbString) {
-                
-                // Set the session
-                req.session.auth = {userId: result.rows[0].id};
-                // set cookie with a session id
-                // internally, on the server side, it maps the session id to an object
-                // { auth: {userId }}
-                res.send('credentials correct!');
-                
+              if (result.rows.length === 0) {
+                  res.status(403).send('username/password is invalid');
               } else {
-                res.status(403).send('username/password is invalid');
+                  // Match the password
+                  var dbString = result.rows[0].password;
+                  var salt = dbString.split('$')[2];
+                  var hashedPassword = hash(password, salt); // Creating a hash based on the password submitted and the original salt
+                  if (hashedPassword === dbString) {
+                    
+                    // Set the session
+                    req.session.auth = {userId: result.rows[0].id};
+                    // set cookie with a session id
+                    // internally, on the server side, it maps the session id to an object
+                    // { auth: {userId }}
+                    res.send('credentials correct!');
+                    
+                  } else {
+                    res.status(403).send('username/password is invalid');
+                  }
               }
           }
-      }
-   });
+       });
+       }
+   else {
+        res.status(403).send('Invalid Username/Password Values!');
+   }
 });
 
 app.post('/submit-comment/:articleName', function (req, res) {
